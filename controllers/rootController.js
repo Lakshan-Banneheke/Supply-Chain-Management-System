d5058c983a2ccf3ddf9d50cf8d3bea35d7202ebe
Lakshan <btlakshan@gmail.com>
2021-01-14 23:30:28 +0530

    Get user's name to dashboard, fix log out successful alert, added </ul> and </div> in _sidebar.ejs

--- controllers/rootController.js
+++ controllers/rootController.js
@@ -3,7 +3,7 @@ const userService = require('../services/UserServices')
 class RootController {
     static async root(req, res) {
         //res.render('index');
-        res.render('dashboard')
+        res.render('dashboard', {name: req.user.name});
     }
 }
 
