7c644a8c700978cf13ac0ac9e1b923f5e7e08b1f
Uditha Isuranga <udithai.18@cse.mrt.ac.lk>
2021-01-22 17:56:11 +0530

    Initiate Supervisor files

---
+++ controllers/supervisorController.js
@@ -0,0 +1,1 @@
+const supervisorModel = require('../model/supervisorModel')
--- routes/root.js
+++ routes/root.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
 const RootController = require('../controllers/rootController');
 
+const supervisorController = require('../controller/supervisorController')
+
 router.get('/', RootController.root);
+
+route.get("/create-new-reqP", supervisorController.renderCreateNewReqP)
+route.post("/create-new-reqS", supervisorController.renderCreateNewReqS)
+
 
 module.exports = router;
