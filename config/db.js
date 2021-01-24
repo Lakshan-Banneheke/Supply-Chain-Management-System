d1391cbee555ff4e67275476a646f2d8287fe76c
Lakshan <btlakshan@gmail.com>
2021-01-24 22:11:47 +0530

    Bug fix in config for hosting

--- config/db.js
+++ config/db.js
@@ -4,6 +4,9 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = pool;
