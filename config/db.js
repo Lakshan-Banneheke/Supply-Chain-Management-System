b7d0eaf67304ac099ec143d2f6018c6686d33e15
Lakshan Banneheke <62496951+Lakshan-Banneheke@users.noreply.github.com>
2021-02-25 17:24:39 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,9 +4,6 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: process.env.NODE_ENV === 'production' ? {
-        rejectUnauthorized: false
-    } : null
 });
 
 module.exports = pool;
